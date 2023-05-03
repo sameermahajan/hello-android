6bf62d3c7f6062396bbbd2122eef237a2b99f3d8
Sameer Mahajan <sameer.s.mahajan@gmail.com>
2023-05-03 14:26:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print ('Hello Android!')
+import kivy
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+    def build(self):
+        return Label(text='Hello Android!')
+
+if __name__ == '__main__':
+    MyApp().run()
